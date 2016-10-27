8d648d1cc4fddb58fa835a41602cf671fcaa8bed
Ania Lisowska <lisowska.a@gmail.com>
2016-10-27 09:34:18 +0100

    I added tabs.

--- superyachts.js
+++ superyachts.js
@@ -1,30 +1,51 @@
 
-  $( function() {
-    var handle = $( "#custom-handle" );
-    $( "#slider" ).slider({
-      create: function() {
-        handle.text( $( this ).slider( "value" ) );
-      },
-      slide: function( event, ui ) {
-        handle.text( ui.value );
-      }
-    });
-  } 
   
   
-  $('#slider').slider({
-	min: 0,
-	max: 100,
-	value: 50,
-	step: 10 // sliderAccess will inherit this
-}).sliderAccess({
-	touchonly : false,
-	where: 'before'
-});
+  //--> script tabs
+  
+  // Wait until the DOM has loaded before querying the document
+			$(document).ready(function(){
+				
+				$('ul.tabs').each(function(){
+					// For each set of tabs, we want to keep track of
+					// which tab is active and its associated content
+					var $active, $content, $links = $(this).find('a');
 
+					// If the location.hash matches one of the links, use that as the active tab.
+					// If no match is found, use the first link as the initial active tab.
+					$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+					$active.addClass('active');
 
+					$content = $($active[0].hash);
 
-$('#basic_example_1').slider().sliderAccess({
-						touchonly : false
+					// Hide the remaining content
+					$links.not($active).each(function () {
+						$(this.hash).hide();
 					});
+
+					// Bind the click event handler
+					$(this).on('click', 'a', function(e){
+						// Make the old tab inactive.
+						
+						$active.removeClass('active');
+						$content.hide();
+
+						// Update the variables with the new link and content
+						$active = $(this);
+						$content = $(this.hash);
+
+						// Make the tab active.
+						$active.addClass('active');
+						$content.show();
+
+						// Prevent the anchor's default click action
+						e.preventDefault();
+					});
+				});
+			});
+			
+  
+    $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
   
